139221ce4e991518a23994933067c5c59e3ad34f
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-04-13 13:47:40 -0700

    replaced to modular version

--- public/app.js
+++ public/app.js
@@ -1,4 +1,19 @@
+const auth = getAuth(app); // firebase authentication
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const userDetails = document.getElementById('userDetails');
+
+const provider = new GoogleAuthProvider();
+
+signInBtn.onclick = () => signInWithPopup(auth, provider);
+signOutBtn.onclick = () => signOut(auth);
+
 document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
-    console.log(app); // gives object of all firebase credentials. checks if firebase is working
+    console.log(app); // gives object of all firebase credentials. checks if firebase is working 
 })
